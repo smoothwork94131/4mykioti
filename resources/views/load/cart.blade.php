eb3cb0489debadc1a6662da8f0ca45d3eca4c12d
smoothwork94131 <developeralex1994131@gmail.com>
2023-03-14 00:39:45 +0900

    carts dialog change

--- resources/views/load/cart.blade.php
+++ resources/views/load/cart.blade.php
@@ -12,22 +12,36 @@
         <ul class="dropdown-cart-products">
             @foreach(Session::get('cart')->items as $product)
                 <li class="product cremove{{ ($product['db']??'products').$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values']) }}">
-                    <div class="product-details">
-                        <div class="content">
-                            <a href="{{ route('front.product',$product['item']->slug) }}"><h6
-                                        class="product-title">{{mb_strlen($product['item']->name,'utf-8') > 45 ? mb_substr($product['item']->name,0,45,'utf-8').'...' : $product['item']->name}}</h6>
-                            </a>
-
-                            <span class="cart-product-info">
-																		<span class="cart-product-qty"
-                                                                              id="cqt{{$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values'])}}">{{$product['qty']}}</span><span>{{ $product['item']->measure }}</span>
-																		x <span
+                    <a href="{{ route('front.product',$product['item']->slug) }}">
+                        <div class="product-details">
+                            <div class='img'>
+                                <a href="{{ route('front.product', $product['item']->slug) }}" class="product-image">
+                                    <img src="{{ $product['item']->photo ? filter_var($product['item']->photo, FILTER_VALIDATE_URL) ?$product['item']->photo:asset('assets/images/products/'.$product['item']->photo):asset('assets/images/noimage.png') }}"
+                                        alt="product">
+                                </a>
+                            </div>
+                            
+                            <div class="content">
+                                <h6
+                                            class="product-title">{{mb_strlen($product['item']->name,'utf-8') > 45 ? mb_substr($product['item']->name,0,45,'utf-8').'...' : $product['item']->name}}</h6>
+                                
+                                <span class="cart-product-info">
+                                    <span class="cart-product-qty"
+                                            id="cqt{{$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values'])}}">{{$product['qty']}}</span><span>{{ $product['item']->measure }}</span>
+                                    x <span
                                         id="prct{{$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values'])}}">{{ App\Models\Product::convertPrice($product['item']->price) }}</span>
-																	</span>
-                        </div>
+                                    </span>
+                            </div>
+                            <div class="cart-remove"
+                                data-class="cremove{{($product['db']??'products').$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values']) }}"
+                                data-href="{{ route('product.cart.remove',($product['db']??'products').$product['item']->id.$product['size'].$product['color'].str_replace(str_split(' ,'),'',$product['values'])) }}"
+                                title="Remove Product">
+                                <i class="icofont-close"></i>
+                            </div>
+                    </a>
                     </div><!-- End .product-details -->
 
-                    <figure class="product-image-container">
+                    <!-- <figure class="product-image-container">
                         <a href="{{ route('front.product', $product['item']->slug) }}" class="product-image">
                             <img src="{{ $product['item']->photo ? filter_var($product['item']->photo, FILTER_VALIDATE_URL) ?$product['item']->photo:asset('assets/images/products/'.$product['item']->photo):asset('assets/images/noimage.png') }}"
                                  alt="product">
@@ -38,7 +52,8 @@
                              title="Remove Product">
                             <i class="icofont-close"></i>
                         </div>
-                    </figure>
+                    </figure> -->
+
                 </li><!-- End .product -->
             @endforeach
         </ul><!-- End .cart-product -->
@@ -47,9 +62,9 @@
             <span>{{ $langg->lang6 }}</span>
 
             <span class="cart-total-price">
-															<span class="cart-total">{{ Session::has('cart') ? App\Models\Product::convertPrice(Session::get('cart')->totalPrice) : '0.00' }}
-															</span>
-														</span>
+                <span class="cart-total">{{ Session::has('cart') ? App\Models\Product::convertPrice(Session::get('cart')->totalPrice) : '0.00' }}
+                </span>
+            </span>
         </div><!-- End .dropdown-cart-total -->
 
         <div class="dropdown-cart-action">
