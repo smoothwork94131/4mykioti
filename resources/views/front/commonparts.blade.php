ccd9f18818835ec5492baa95f93ce14efb5340f0
smoothwork94131 <developeralex1994131@gmail.com>
2023-03-22 04:43:11 +0900

    back category

--- resources/views/front/commonparts.blade.php
+++ resources/views/front/commonparts.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.front')
 @section('content')
-
+    <?php 
+        $name = "" ;
+        $group_id = "" ;
+       
+    ?>
     <!-- Breadcrumb Area Start -->
     <div class="breadcrumb-area">
         <div class="container">
@@ -14,7 +18,7 @@
                         </li>
                         <li>
                             <a href="javascript:location.reload();">
-                                Tractors
+                                Categories
                             </a>
                         </li>
                     </ul>
@@ -22,26 +26,82 @@
             </div>
         </div>
     </div>
-    <!-- Breadcrumb Area End -->
-    <!-- faq Area Start -->
 
     <input type="hidden" id="isSchematics" value="0">
-
     <section class="faq-section">
         <div class="container">
-            
             <div class="row m-block-content">
+            @if($type == "category")
                 @foreach($eccategories as $key => $item)
-                    <div class="col col-md-3 col-sm-4">
-                        <div class="m-block" data-type="category"
-                            data-series="{{$item->id}}"
-                            data-url="{{route('front.groups')}}"
-                            data-category-name="{{$item->name}}"
-                            data-status="0" data-token="{{ csrf_token() }}">
-                            {{$item->name}}
+                        <div class="col col-md-3 col-sm-4">
+                            <div class="m-block" 
+                                data-type="category"
+                                data-series="{{$item->id}}"
+                                data-url="{{route('front.groups')}}"
+                                data-category-name="{{$item->name}}"
+                                data-page="commonparts"
+                                data-status="0" data-token="{{ csrf_token() }}">
+                                {{$item->name}}
+                            </div>
                         </div>
-                    </div>
                 @endforeach
+            @else 
+                @foreach($page_categories as $item)
+                        <?php 
+                            
+                            if($type == "model") {
+                                $item->series = $item->name ;
+                                $item->model="" ;
+                                $name = $item->name ;
+                                $item->section_name = "" ;
+                                $item->group_name = "" ;
+                                $series = $item->name ;
+
+                                $series = $item->name ;
+                                
+
+                            } else if($type == "section") {
+                                $item->series = $series;
+                                $name = $item->model ;
+                                $item->group_name = "" ;
+                                $item->section_name = "" ;
+
+                            } else if($type == "group") {
+                                $name = $item->section_name ;
+                                $item->model = $model ;
+                                $item->series = $series ;
+                                $item->group_name = "" ;
+                                
+                            } else if($type == "detail") {
+                                $name = $item->group_name ;
+                                $item->model = $model ;
+                                $item->series = $series ;
+                                $item->group_name = $name ;
+                                $item->section_name = "" ;
+                                $group_id = $item->group_Id ;
+                            }
+                        ?>
+                        
+                        <div class="col col-md-3 col-sm-4">
+                            <div class="m-block" 
+                                data-type="{{$type}}"
+                                data-series="{{$item->series}}"
+                                data-url="{{route('front.groups')}}"
+                                data-model="{{$item->model}}"
+                                data-section="{{$item->section_name}}"
+                                data-groupname="{{$item->group_name}}"
+                                data-group="{{$group_id}}"
+                                data-page="commonparts"
+                                data-status="0" data-token="{{ csrf_token() }}">
+                                {{$name}}
+                            </div>
+                        </div>
+                @endforeach
+                @if(count($page_categories) == 0)
+                    <h3 align='center'>No Data</h3>
+                @endif
+            @endif  
+                
             </div>
         </div>
     </section>
--- resources/views/front/schematics.blade.php
+++ resources/views/front/schematics.blade.php
@@ -30,17 +30,72 @@
     <section class="faq-section">
         <div class="container">
             <div class="row m-block-content">
-                     @foreach($eccategories as $key => $item)
-                            <div class="col col-md-3 col-sm-4">
-                                <div class="m-block" data-type="category"
-                                                data-series="{{$item->id}}"
-                                                data-url="{{route('front.groups')}}"
-                                                data-category-name="{{$item->name}}"
-                                                data-status="0" data-token="{{ csrf_token() }}">
-                                                {{$item->name}}
-                                </div>
+            @if($type == "category")
+                @foreach($eccategories as $key => $item)
+                        <div class="col col-md-3 col-sm-4">
+                            <div class="m-block" 
+                                data-type="category"
+                                data-series="{{$item->id}}"
+                                data-url="{{route('front.groups')}}"
+                                data-category-name="{{$item->name}}"
+                                data-page="schematics"
+                                data-status="0" data-token="{{ csrf_token() }}">
+                                {{$item->name}}
                             </div>
-                    @endforeach
+                        </div>
+                @endforeach
+            @else
+                @foreach($page_categories as $item)
+                        <?php 
+                            $name = "" ;
+                            $group_id = "" ;
+                            if($type == "model") {
+                                $item->series = $item->name ;
+                                $item->model="" ;
+                                $name = $item->name ;
+                                $item->section_name = "" ;
+                                $item->group_name = "" ;
+                            } else if($type == "section") {
+                                $item->series = $series;
+                                $name = $item->model ;
+                                $item->group_name = "" ;
+                                $item->section_name = "" ;
+                            } else if($type == "group") {
+                                $name = $item->section_name ;
+                                $item->model = $model ;
+                                $item->series = $series ;
+                                $item->group_name = "" ;
+                                
+                            } else if($type == "detail") {
+                                $name = $item->group_name ;
+                                $item->model = $model ;
+                                $item->series = $series ;
+                                $item->group_name = $name ;
+                                $item->section_name = "" ;
+                                $group_id = $item->group_Id ;
+                            }
+                        ?>
+                        
+                        <div class="col col-md-3 col-sm-4">
+                            <div class="m-block" 
+                                data-type="{{$type}}"
+                                data-series="{{$item->series}}"
+                                data-url="{{route('front.groups')}}"
+                                data-model="{{$item->model}}"
+                                data-section="{{$item->section_name}}"
+                                data-groupname="{{$item->group_name}}"
+                                data-group="{{$group_id}}"
+                                data-page="schematics"
+                                data-status="0" data-token="{{ csrf_token() }}">
+                                {{$name}}
+                            </div>
+                        </div>
+                @endforeach
+            @endif
+            @if(count($page_categories) == 0)
+                <h3 align='center'>No Data</h3>
+            @endif
+                
             </div>
         </div>
     </section>
