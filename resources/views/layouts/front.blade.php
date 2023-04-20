bc5a8e3eb63c37d6c1736cdb4954a2df9317dc9e
smoothwork94131 <developeralex1994131@gmail.com>
2023-04-21 06:30:53 +0800

    fixed spinner issues

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -63,7 +63,7 @@
 
     @if ($gs->is_loader == 1)
     <div class="preloader" id="preloader"
-        style="background: url({{ asset('assets/images/' . $gs->loader) }}) no-repeat scroll center center #FFF;">
+        style="background: #FFF;">
     </div>
     @endif
 
