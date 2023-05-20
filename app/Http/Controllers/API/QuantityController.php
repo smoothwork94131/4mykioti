c815b56c85565a23337e3c20b9d8300219449624
smoothwork94131 <developeralex1994131@gmail.com>
2023-05-20 09:46:57 -0400

    QuantityController foreach update

--- app/Http/Controllers/API/QuantityController.php
+++ app/Http/Controllers/API/QuantityController.php
@@ -35,15 +35,12 @@ class QuantityController extends Controller
                 foreach ($series as $serie) {
                     $table = strtolower($serie->name);
 
-                    foreach ($parts as $part) {
-                        $sku = $part["sku"];
-                        $quantity = $part["quantity"];
-                        $result = $connection->table($table)
-                            ->where('sku', $sku)
-                            ->update([
-                                'stock' => DB::raw('stock - ' . (int)$quantity)
-                            ]);
-                    }
+                    $result = $connection->table($table)
+                        ->where('sku', $sku)
+                        ->update([
+                            'stock' => DB::raw('stock - :quantity'),
+                            'bindings' => ['quantity' => (int)$quantity],
+                        ]);
                 }
             }
 
