fa5eb35991a9b33fa70f8a8f5150e30b9ff095ad
smoothwork94131 <developeralex1994131@gmail.com>
2023-05-20 09:51:24 -0400

    Update CheckApiIp add single quote

--- app/Http/Middleware/CheckApiIp.php
+++ app/Http/Middleware/CheckApiIp.php
@@ -16,7 +16,7 @@ class CheckApiIp
      */
     public function handle(Request $request, Closure $next)
     {
-        $allowedIps = ['127.0.0.1', '24.239.36.98', '97.107.230.39', 96.235.34.206, 193.69.136.134]; // Replace with your own whitelist
+        $allowedIps = ['127.0.0.1', '24.239.36.98', '97.107.230.39', '96.235.34.206', '193.69.136.134']; // Replace with your own whitelist
 
         $ip = $request->ip();
 
