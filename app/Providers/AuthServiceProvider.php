51dc4e082469276cca8acd40b9c72052cef8a7ac
smoothwork94131 <developeralex1994131@gmail.com>
2023-06-01 01:38:30 +0200

    fixed limit per min

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         RateLimiter::for("login", function () {
-            Limit::perMinute(120);
+            Limit::perMinute(60);
         });
     }
 }
